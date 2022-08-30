0a1d33577c07fb72ad5afcc33b33565ef69ad8a3
jiaxingjeanneyu <jiaxinguy@gmail.com>
2022-08-30 19:26:02 -0400

    add intro

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome To Jekyll",
+        "excerpt":"jiaxing_yu jy729@rutgers.edu; jiaxinguy@gmail.com More About Me My linguistics interests are in semantics, syntax and experimental linguistics. My master’s thesis topic is The Syntactic and Semantic Analysis of Classifier-noun phrases in Mandarin, with emphasis on lexical semantics of classifiers. I enjoy using mathematical ways to represent the internal structure and meanings...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/"
+      }]
